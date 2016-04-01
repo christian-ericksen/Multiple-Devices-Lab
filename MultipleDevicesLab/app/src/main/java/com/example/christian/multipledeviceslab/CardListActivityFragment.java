44736235f771946fc303cbb121f868e3e99471d7
christian-ericksen <christian.ericksen@gmail.com>
2016-04-01 11:54:03 -0400

    Final Commit

--- MultipleDevicesLab/app/src/main/java/com/example/christian/multipledeviceslab/CardListActivityFragment.java
+++ MultipleDevicesLab/app/src/main/java/com/example/christian/multipledeviceslab/CardListActivityFragment.java
@@ -1,12 +1,10 @@
 package com.example.christian.multipledeviceslab;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.christian.multipledeviceslab.R;
 
 /**
  * A placeholder fragment containing a simple view.
--- MultipleDevicesLab/app/src/main/java/com/example/christian/multipledeviceslab/MainActivity.java
+++ MultipleDevicesLab/app/src/main/java/com/example/christian/multipledeviceslab/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.christian.multipledeviceslab;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
